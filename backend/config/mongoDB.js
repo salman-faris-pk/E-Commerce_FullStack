cb42450e59fc7d49ab3fc5a3a0a96e01b975604a
Salmanul FarisPK <salmanulfarispk1405@gmail.com>
2024-10-26 20:27:39 +0530

    over

--- backend/config/mongoDB.js
+++ backend/config/mongoDB.js
@@ -8,10 +8,7 @@ const connectDB =async()=>{
      mongoose.connection.on("connected",()=> {
         console.log("DB connected"); 
      })
-    await mongoose.connect(`${process.env.MONGODB_URI}/e-commerce`,{
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    })
+    await mongoose.connect(`${process.env.MONGODB_URI}/e-commerce`)
 }
 
 
